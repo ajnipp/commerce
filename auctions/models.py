ce5dedb5296da089ed1ef84b4f6015c4e85f764e
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2023-06-04 16:44:27 -0400

    Created listing model

--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,21 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    # Extends abstract user, so it comes with most default behavior.
+    id = models.AutoField(primary_key=True)
+
+class Listing(models.Model):
+    id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField(auto_now_add=True) # Holds the timestamp for the listing's creation
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image_url = models.URLField(blank=True)
+    CATEGORIES = [
+        ('TOY', 'Toys'),
+        ('ELC', 'Electronics'),
+        ('HOM', 'Home'),
+        ('TOO', 'Tools'),
+    ]
+    category = models.CharField(max_length=3, choices=CATEGORIES, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
