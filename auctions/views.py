beecffecdcb072397cae26b3ee28cbaf87946520
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2023-06-08 18:59:23 -0400

    form validation working for bid

--- auctions/views.py
+++ auctions/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseBadRequest 
 from django.shortcuts import render
 from django.urls import reverse
 from django import forms 
 from django.utils.translation import gettext_lazy as _
 
-from .models import User, Listing
+from .models import User, Listing, Bid
 
 
 def index(request):
@@ -93,11 +93,24 @@ def add_listing(request):
     return render(request, 'auctions/add_listing.html', {
         'form': ListingForm() 
     })
+
+class BidForm(forms.ModelForm):
+    class Meta:
+        model = Bid 
+        fields = ['amount']
+    def clean(self):
+        cleaned_data = super().clean()
+        amount = cleaned_data['amount']
+        print(amount)
+        if amount <= self.instance.listing.price:
+            print("value too small")
+            self.add_error('amount', "Bid must be higher than the current price!")
+
 def listing(request, listing_id):
     user = request.user
     listing = Listing.objects.get(pk=listing_id)
     if listing is None:
-        pass
+        return Http404('Listing not found!') 
     is_watching = listing in user.watchlist.all()
     bids = listing.bids.all()
     bid_count = bids.count()
@@ -106,12 +119,31 @@ def listing(request, listing_id):
         is_highest_bidder = False
     else:
         is_highest_bidder = highest_bid.bidder == user
+    if request.method == 'POST':
+        if user == listing.owner:
+            return HttpResponse('Cannot post bid as the owner!') 
+        bid = Bid(listing=listing, bidder=user)
+        form = BidForm(request.POST, instance=bid) 
+        if form.is_valid():
+            form.save()
+            listing.price = form.cleaned_data['amount']
+            listing.save(update_fields=['price'])
+        else:
+            return render(request, 'auctions/listing.html', {
+                'user': user,
+                'listing': listing,
+                'is_watching': is_watching,
+                'bid_count': bid_count,
+                'is_highest_bidder': is_highest_bidder,
+                'form': form
+            })
     return render(request, 'auctions/listing.html', {
         'user': user,
         'listing': listing,
         'is_watching': is_watching,
         'bid_count': bid_count,
         'is_highest_bidder': is_highest_bidder,
+        'form': BidForm()
     })
 
 def categories(request):
