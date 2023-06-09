fb92ad8eb34b81fded1225a2570f6dbe22e393ee
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2023-06-09 13:55:18 -0400

    added files and registered Comment

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Listing, Bid
+from .models import User, Listing, Bid, Comment
 # Register your models here.
 
-admin.site.register([User, Listing, Bid])
+admin.site.register([User, Listing, Bid, Comment])
